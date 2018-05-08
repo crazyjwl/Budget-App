1b169ef7b1ad51890bbd641517f9f188d43be13e
crazyjwl <crazyjwl.163.com>
2018-05-08 17:42:08 +0800

    Updating the budget : budget controller

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ var budgetController = (function() {
     this.value = value;
   };
 
+  var calculateTotal = function (type){
+    var sum = 0;
+    data.allItems[type].forEach(function(cur) {
+      sum += cur.value;
+    });
+    data.totals[type] = sum;
+  };
+
   var data = {
     allItems :{
       inc : [],
@@ -23,7 +31,9 @@ var budgetController = (function() {
     totals : {
       inc : 0,
       exp : 0
-    }
+    },
+    budget : 0,
+    percentage : -1
   }
 
   return {
@@ -52,6 +62,34 @@ var budgetController = (function() {
 
       //return the new element
       return newItem;
+    },
+
+    calculateBudge : function() {
+
+      // calculate the total income and expense
+      calculateTotal('inc');
+      calculateTotal('exp');
+
+      // calculate the budget : income - expense
+      data.budget = data.totals.inc - data.totals.exp;
+
+      // calculate the percentage of income that spent
+      if (data.totals.inc > 0) {
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+      } else {
+        data.percentage = -1;
+      }
+
+
+    },
+
+    getBudget : function(){
+      return {
+        budget : data.budget,
+        totalInc : data.totals.inc,
+        totalExp : data.totals.exp,
+        percentage : data.percentage
+      }
     },
 
     testing : function(){
@@ -146,10 +184,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   var updateBudget = function(){
     //1. Calculate the budget
+    budgetCtrl.calculateBudge();
 
     //2. Return the budget
+    var budget = budgetCtrl.getBudget();
 
     //3. Display the budget on the UI
+    console.log(budget);
   };
 
   var crtlAddItem = function(){
