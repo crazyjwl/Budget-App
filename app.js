69a93a69f54892b5677284476e7983275d71bed8
crazyjwl <crazyjwl.163.com>
2018-05-08 14:17:58 +0800

    Setting up the first EventListener

--- app.js
+++ app.js
@@ -20,5 +20,25 @@ var UIController = (function() {
 
 var controller = (function(bugetCtrl, UICtrl) {
 
+  var crtlAddItem = function(){
+
+    //1. Get the field input data
+
+    //2. Add the item to the budget controller
+
+    //3. Add the item into the UI
+
+    //4. Calculate the budget
+
+    //5. Display the budget on the UI
+
+  }
+
+  document.querySelector('.add_btn').addEventListener('click', crtlAddItem);
+  document.addEventListener('keypress', function(e){
+    if (e.keyCode === 13 || e.witch === 13) {
+      crtlAddItem();
+    }
+  });
 
 })(budgetController, UIController);
