f4ae5bbf17f9c3fcc4d882f513fb944c1101cbb7
crazyjwl <crazyjwl.163.com>
2018-05-08 15:47:50 +0800

    Creating Income and Expense Function

--- app.js
+++ app.js
@@ -3,6 +3,28 @@
 **/
 var budgetController = (function() {
 
+  var Income = function(id, description, value){
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Expense = function(id, description, value){
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems :{
+      inc : [],
+      exp : []
+    },
+    totals : {
+      inc : 0,
+      exp : 0
+    }
+  }
 
 })();
 
