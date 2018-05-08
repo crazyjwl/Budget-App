ac070541ae653105518eb6112f3a2b7602610078
crazyjwl <crazyjwl.163.com>
2018-05-08 15:35:27 +0800

    Reading input data

--- app.js
+++ app.js
@@ -11,7 +11,25 @@ var budgetController = (function() {
 **/
 var UIController = (function() {
 
+  var DOMStrings = {
+    inputType : '.add_type',
+    inputDescription : '.add_description',
+    inputValue : '.add_value',
+    inputBtn : '.add_btn'
+  }
 
+  return {
+    getInput : function(){
+      return {
+        type : document.querySelector(DOMStrings.inputType).value, //this will be either inc or exp
+        description : document.querySelector(DOMStrings.inputDescription).value,
+        value : document.querySelector(DOMStrings.inputValue).value
+      }
+    },
+    getDOMStrings : function(){
+      return DOMStrings;
+    }
+  };
 })();
 
 /**
@@ -20,10 +38,13 @@ var UIController = (function() {
 
 var controller = (function(bugetCtrl, UICtrl) {
 
+  var DOM = UICtrl.getDOMStrings();
+
   var crtlAddItem = function(){
 
     //1. Get the field input data
-
+    var input = UICtrl.getInput();
+    console.log(input);
     //2. Add the item to the budget controller
 
     //3. Add the item into the UI
@@ -34,7 +55,7 @@ var controller = (function(bugetCtrl, UICtrl) {
 
   }
 
-  document.querySelector('.add_btn').addEventListener('click', crtlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', crtlAddItem);
   document.addEventListener('keypress', function(e){
     if (e.keyCode === 13 || e.witch === 13) {
       crtlAddItem();
